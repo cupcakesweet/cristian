c5fb34da8b6d0ddf0269809d2efb35cd4a1dda8f
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 15:51:09 -0500

    Update Estructura_cristian.java

--- Estructura_cristian.java
+++ Estructura_cristian.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.estructura_cristian;
+import java.util.Scanner;
+import java.io.*;
 
-/**
- *
- * @author SenaManana
- */
 public class Estructura_cristian {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+ public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.print("Inserte un numero: ");
+        x = input.nextInt();
+        if (x < 0)
+            x=x*-1;
+        System.out.println("El numero es: "+x);
     }
 }
+   
