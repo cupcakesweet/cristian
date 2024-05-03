95b8e75d43d560b01f394e3ff632d6e564343394
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 15:31:38 -0500

    Add files via upload

---
+++ Ciclos_cristian.java
@@ -0,0 +1,36 @@
+package com.mycompany.ciclos_cristian;
+import javax.swing.JOptionPane;
+public class Ciclos_cristian {
+
+   public static void main(String[] args) {
+        int x=0;
+        do {
+            JOptionPane.showMessageDialog(null, "Valor de x: "+x);
+            x++;
+        } while (x<=10);
+        
+        while (x<=10) {
+            JOptionPane.showMessageDialog(null, "El valor de X es: "+x);
+        }
+        
+        for (int i=0;i<=10;i++) {
+            JOptionPane.showMessageDialog(null, "El valor de i es: "+i);
+        }
+        
+        int i = 0;
+        boolean condition = true;
+        for (;condition;) {
+            if (i<=10) {
+                JOptionPane.showMessageDialog(null, "El valor de i es: "+i);
+                i++;
+                condition=true;
+            } else {
+                condition=false;
+            }
+        }
+    }
+}
+/*
+Cada ejemplo imprime el resultado en consola, repita cada uno de ellos pero ahora imprimiendo en pantalla usando para eso
+un JOptionPane.
+*/
