8372c1dad1fc3583a6349fbc99da534365928683
eduard_cupcakes <161329071+cupcakesweet@users.noreply.github.com>
2024-05-03 15:22:36 -0500

    Add files via upload

---
+++ tarea_3.java
@@ -0,0 +1,21 @@
+package com.mycompany.estructura_cristian;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class tarea_3 {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int boughtItems;
+        float price, totalPrice;
+        System.out.print("Ingrese la cantidad de items comprados: ");
+        boughtItems = input.nextInt();
+        System.out.print("Ingrese el precio de los items individuales: ");
+        price = input.nextFloat();
+        totalPrice = boughtItems * price;
+        if (totalPrice > 50000) {
+            totalPrice = (totalPrice / 100) * 110;
+        }
+        System.out.println("Precio final: "+totalPrice+"$");
+    }
+}
